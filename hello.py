dee3ea1f397ae47961de6da12edd2a5437907daa
Akash Verma <akash.verma.sec@gmail.com>
2026-07-07 17:15:18 +0530

    Day 1: Python Basics - Hello, Variables, Input

---
+++ hello.py
@@ -0,0 +1,28 @@
+print("HELLO, Akash!")
+print("Welcome to the python")
+print("Future Cloud Security Engineer")
+
+# Variables
+name = "Akash"
+age = 22
+course = "MCA"
+print("name :", name)
+print("age :", age)
+print("course :", course)
+
+# User Input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Welcome", name)
+print("Your age is", age)
+
+# Data Types
+name = "Akash"  # String
+age = 22  # Integer
+height = 5.9  # Float
+is_student = True  # Boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
