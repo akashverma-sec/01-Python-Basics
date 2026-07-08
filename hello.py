7739429948aa74b3d3a08ad58b6630a29162878b
Akash Verma <akash.verma.sec@gmail.com>
2026-07-08 19:08:41 +0530

    Day 2: Python Operators and Comparison Operators

--- hello.py
+++ hello.py
@@ -26,3 +26,4 @@ print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_student))
+
---
+++ operators.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+a = 20
+b = 5
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+print("Power =", a ** b)
+print("Floor Division =", a // b)
+
+# Comparison Operators
+
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
