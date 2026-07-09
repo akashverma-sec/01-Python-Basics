f5186a2c98a493ffd6865a811eb8329c86200a4c
Akash Verma <akash.verma.sec@gmail.com>
2026-07-09 16:47:38 +0530

    Day 3: Python Conditions and Even-Odd Project

---
+++ conditions.py
@@ -0,0 +1,28 @@
+# if statement
+
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote.")
+
+# if-else statement
+
+age = 13 
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+# if-elif-else Statement
+
+marks = 55
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Fail")
---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# Even or Odd Checker
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is Even")
+else:
+    print(number, "is Odd")
