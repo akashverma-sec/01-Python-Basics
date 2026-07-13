f67bde3873c8f8f25e5dc04b2a2135831d409662
Akash Verma <akash.verma.sec@gmail.com>
2026-07-13 18:53:01 +0530

    Day 7: Python Functions and Simple Calculator

---
+++ functions.py
@@ -0,0 +1,25 @@
+# Simple Function
+
+def greet():
+    print("Hello Akash!")
+    print("Welcome to Python Functions.")
+
+greet()
+
+# Function with Arguments
+
+def greet(name):
+    print("Hello", name)
+    print("Welcome to Python!")
+
+greet("Akash")
+greet("Rahul")
+
+# Function with Return
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+
+print("Addition =", result)
