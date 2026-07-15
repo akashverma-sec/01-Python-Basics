612dc2a07ca72abf6ed1a3bb88d43190525dc1f9
Akash Verma <akash.verma.sec@gmail.com>
2026-07-15 18:33:26 +0530

    Day 8: Python Modules and Password Generator

---
+++ modules.py
@@ -0,0 +1,23 @@
+# Math module 
+import math
+
+print("Square Root:", math.sqrt(25))
+print("Power:", math.pow(2, 5))
+print("Pi Value:", math.pi)
+
+# Random module
+import random
+
+print("Random Number:", random.randint(1, 100))
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Random Fruit:", random.choice(fruits))
+
+# Date and Time module
+
+from datetime import datetime
+
+today = datetime.now()
+
+print("Current Date and Time:", today)
